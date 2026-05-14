a36b6704b9fcd981d31f3c354324be04acc85ec5
Alejandro-C12 <alejandro.cueva@udla.edu.ec>
2026-05-14 15:02:23 -0500

    2do commit: Segunda version del codigo para imprimir el vector por puntero

--- 140526.c
+++ 140526.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 int main(){
     int numeros[7] = {3,6,9,12,15,18,21};
+    int *ptr;
+    ptr = &numeros[0];
+
     for (int i=0; i<7; i++){
         printf("%d ", numeros [i]);
+        printf("%p\n", ptr);
     }
     return 0;
 }
